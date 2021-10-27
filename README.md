9e94e5d5cfccc535c84b7c77f56c167b8b11bdfa
Kaushlesh Chandel <kaushleshchandel@gmail.com>
2021-10-27 09:52:14 -0500

    Initial COmmit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # bluevan-jetson4g
 Bluevan for Jetson 4 GB
+
+Install git client:
+
+sudo apt-get update
+sudo apt-get install git
+
+git clone -b bluevan https://github.com/kaushleshchandel/bluevan.git
+
+sudo sh ./bluevan/install.sh
+
+sudo systemctl start bluevan.service
+To automatically start bluevan at boot:
+
+sudo systemctl enable bluevan.service
+To stop bluevan:
+
+sudo systemctl stop bluevan.service
+
+or
+Press "q" key.
---
+++ install.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+if [ `whoami` != root ]; then
+ echo "Please run with sudo!"
+ exit 1
+fi
+
+WORKDIR="$(dirname "$0")"
+cd $WORKDIR
+
+DESTPATH_APPDATA="/usr/local/share/bluevan/"
+DESTPATH_BIN="/usr/local/bin/bluevan"
+SYSTEMD_PATH="/lib/systemd/system/"
+
+# ------ Install files to the correct location -------
+# ----------------------------------------------------
+
+# Copy application
+echo "Copy appdate"
+mkdir -p $DESTPATH_APPDATA
+cp -v -R  * $DESTPATH_APPDATA
+chmod 755 -R $DESTPATH_APPDATA
+
+# Copy executable
+echo "Copy executable"
+cp -v ./bin/bluevan $DESTPATH_BIN
+chmod 755 $DESTPATH_BIN
+
+# Be sure normal users can't read our config file!
+#chmod 600 $DESTPATH_APPDATA"settings.ini"
+
+# --- Install the required distribution packages -----
+# ----------------------------------------------------
+
+echo "Installing required distribution packages"
+apt-get update
+
+if [ ! -e /usr/bin/pip3 ]; then
+    apt-get -y install python3-pip
+fi
+
+#if [ ! -e /usr/bin/ffprobe ]; then
+#    apt-get -y install ffmpeg
+#fi
+
+#if [ ! -e /usr/bin/omxplayer ]; then
+#    apt-get -y install omxplayer
+#fi
+
+# --------- Install required python packages ---------
+# ----------------------------------------------------
+
+echo "Installing required python packages"
+pip3 show evdev 1>/dev/null
+if [ $? != 0 ]; then
+    pip3 install evdev==1.2.0
+fi
+
+# ---------------- Systemd service -------------------
+# ----------------------------------------------------
+
+echo "Installing 'Bluevan' as a systemd service"
+cp -v bluevan.service $SYSTEMD_PATH
+systemctl daemon-reload
+systemctl disable bluevan.service
+
+# ---------------------- pipng -----------------------
+# ----------------------------------------------------
+
+#echo "Installing and building pipng"
+#git clone https://github.com/raspicamplayer/pipng.git
+#cd ./pipng/ && make && make install
+#cd ../
+#rm -rf pipng
+
+# --------------------- Done! ------------------------
+# ----------------------------------------------------
+
+echo "Done!"
