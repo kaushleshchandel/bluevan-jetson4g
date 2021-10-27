2093d1e23caf430cc11ae776d9380b71e91374f1
Kaushlesh Chandel <kaushleshchandel@gmail.com>
2021-10-27 11:14:01 -0500

    Updated the sysmd to load python script

---
+++ remove.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ `whoami` != root ]; then
+ echo "Please run with sudo!"
+ exit 1
+fi
+
+echo "Stopping the 'Bluevan' service"
+systemctl stop bluevan.service
+
+WORKDIR="$(dirname "$0")"
+cd $WORKDIR
+
+# ------ Remove files and service correct location -------
+# ----------------------------------------------------
+
+# Copy application
+echo "Removing Directories"
+rm -r "/usr/local/share/bluevan/"
+rm -r "/usr/local/bin/bluevan"
+
+echo "Removing the 'Bluevan' service"
+systemctl remove bluevan.service
+rm "/lib/systemd/system/bluevan.service"
+systemctl daemon-reload
+
+echo "Done!"
